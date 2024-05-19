5e575a9e07d3976071c0de0c4c5460e7484d7727
ChoppiingBlock <2606935387@qq.com>
2024-05-19 20:04:08 +0800

    在分支上进行修改，和主干不同

--- src/main/java/org/example/gitTest2.java
+++ src/main/java/org/example/gitTest2.java
@@ -2,6 +2,6 @@ package org.example;
 
 public class gitTest2 {
     public static void main(String[] args) {
-        System.out.println("这里是主干，和分支冲突");
+        System.out.println("这里是分支");
     }
 }
